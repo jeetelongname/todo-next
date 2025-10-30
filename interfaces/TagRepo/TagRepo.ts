718385abcec9ce6de6544f48413216d667363322
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2025-10-30 00:58:38 +0000

    feat(models): add interfaces
    This commit adds in all major CRUD interfaces for the three major types
    TODO: Session interface
    
    Refer #1

---
+++ interfaces/TagRepo/TagRepo.ts
@@ -0,0 +1,18 @@
+import { Todo } from "@/models/todo";
+import { Tag, InsertTag } from "@/models/tag";
+
+export interface TagRepo {
+  // Tag CRUD
+  tag_create(tag: InsertTag): Promise<Tag>;
+  tag_read(id: string): Promise<Tag | null>;
+  // no update as tags store only their name
+  tag_delete(id: string): Promise<Boolean>;
+
+  // FIXME: These will need to be constrained to a user
+  // get all todos with a tag.
+  todo_tag_get_todos(tag_id: string): Promise<Array<Todo> | null>;
+  // This function will get all todos with all tags specified
+  todo_tag_get_todos_multiple_tags(
+    tag_ids: string[],
+  ): Promise<Array<Todo> | null>;
+}
---
+++ interfaces/UserRepo/UserRepo.ts
@@ -0,0 +1,18 @@
+import { User, InsertUser } from "@/models/user";
+import { Todo } from "@/models/todo";
+import { Tag } from "@/models/tag";
+
+export default interface UserRepo {
+  // User CRUD
+  user_create(user: InsertUser): Promise<User>;
+  user_read(id: string): Promise<User | null>;
+  user_update(id: string, updates: Partial<User>): Promise<User>;
+  user_delete(id: string): Promise<Boolean>;
+
+  // get all todos associated with a user
+  user_todo_get_todos(user_id: string): Promise<Todo[]>;
+
+  // get all tags a user has used,
+  // thinking atm is a double join but figure out later
+  user_tag_get_all_tags(user_id: string): Promise<Tag[]>;
+}
