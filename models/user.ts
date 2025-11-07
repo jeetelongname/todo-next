49c1a219e27bd48d0e5240e8abe569813c82c647
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2025-11-07 02:11:46 +0000

    fix(model): make pass_hash optional
    in day to day it should not be accessed and is only really there for
    construction, keeping it in the user object is a security risk

--- models/user.ts
+++ models/user.ts
@@ -1,12 +1,12 @@
 export type User = {
   id: string;
   email: string;
-  pass_hash: string;
+  pass_hash?: string;
   name: string;
 };
 
 export type InsertUser = {
   email: string;
-  pass_hash: string;
+  pass_hash?: string;
   name: string;
 };
